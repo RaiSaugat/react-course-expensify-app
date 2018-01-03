014648b3b2ed3de7a79f2ec1a8df65ff93909cd8
raisaugat <rai.saugat.sr@gmail.com>
2018-01-03 19:21:56 +0545

    Remove Button fixed

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { editExpense } from '../actions/expenses';
-import { removeExpense } from '../actions/expenses';
+import { editExpense , removeExpense } from '../actions/expenses';
 
 export class EditExpensePage extends React.Component {
     onSubmit = (expense) => {
@@ -24,15 +23,15 @@ export class EditExpensePage extends React.Component {
             </div>
         );
     }
-}
+};
 
 const mapStateToProps = (state, props) => ({
     expense: state.expenses.find((expense) => expense.id === props.match.params.id)
 });
 
-const mapDispatchToProps = (dispatch, props) => {
-    editExpense: (id, expense) => dispatch(editExpense(id, expense));
-    removeExpense: (id) => dispatch(removeExpense(id));
-};
+const mapDispatchToProps = (dispatch, props) => ({
+    editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+    removeExpense: (data) => dispatch(removeExpense(data))
+});
 
-export default connect(mapStateToProps, )(EditExpensePage);
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
