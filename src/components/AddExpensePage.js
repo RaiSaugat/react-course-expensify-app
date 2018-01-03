37ce28a1bb782a8c15dc2b73742f3f6e4988300f
raisaugat <rai.saugat.sr@gmail.com>
2018-01-03 17:30:05 +0545

    Setup formatting for amount and createdAt

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -24,4 +24,4 @@ const mapDispatchToProps = (dispatch) => ({
     addExpense: (expense) => dispatch(addExpense(expense))
 });
 
-export default connect(undefined, )(AddExpensePage );
+export default connect(undefined, mapDispatchToProps)(AddExpensePage );
